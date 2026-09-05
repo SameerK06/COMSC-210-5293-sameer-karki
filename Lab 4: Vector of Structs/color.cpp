cea8e29825fb89158abdb782bdf9337a5858b82b
Sameer Karki <sameerkarkeetheone@gmail.com>
2026-09-05 10:06:06 -0700

    Lab 4; Milestone 2: Create a vector and display the vector

--- Lab 4: Vector of Structs/color.cpp
+++ Lab 4: Vector of Structs/color.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -15,8 +16,21 @@ struct Color {
 };
 
 int main() {
+    // Milestone 1: Create a Color object and display its values
     Color color = {255, 0, 0};
+    cout << "Milestone 1: Displaying a single Color object:" << endl;
     color.displayColor();
+
+    // Milestone 2: Create an vector of Color objects and display their values
+    vector<Color> colors;
+    for(int i = 0; i < rand() % 6 + 25; i++) {
+        Color newColor = {rand() % 256, rand() % 256, rand() % 256};
+        colors.push_back(newColor);
+    }
+    cout << endl << "Milestone 2: Displaying a vector of Color objects:" << endl;
+    for(const auto& c : colors) {
+        c.displayColor();
+    }
 
     return 0;
 }
