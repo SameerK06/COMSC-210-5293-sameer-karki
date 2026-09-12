e1d3177dfc5ec82f5bb2d076883184beb373bf14
Sameer Karki <sameerkarkeetheone@gmail.com>
2026-09-12 15:52:53 -0700

    Lab 7: Testing

--- Lab 7: Dynamic Arrays & Functions II/array.cpp
+++ Lab 7: Dynamic Arrays & Functions II/array.cpp
@@ -7,6 +7,15 @@ void displayArray(const string* arr, int size);
 string* reverseArray(string* arr, int size);
 
 int main() {
+    const int SIZE = 5;
+    string* arr = new string[SIZE]{"Bob", "bob1", "bob2", "bob3", "bob4"};
+    cout << "Original array: ";
+    displayArray(arr, SIZE);
+    cout << endl;
+    cout << "Reversed array: ";
+    displayArray(reverseArray(arr, SIZE), SIZE);
+    delete[] arr;
+    arr = nullptr;
     return 0;
 }
 
