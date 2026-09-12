95843b2cf8d70a90e44b9bfd407ad62de325132e
Sameer Karki <sameerkarkeetheone@gmail.com>
2026-09-12 10:14:04 -0700

    Lab 6: updated the outputArrayData to look cleaner

--- Lab 6: Dynamic Arrays & Functions I/array.cpp
+++ Lab 6: Dynamic Arrays & Functions I/array.cpp
@@ -18,9 +18,9 @@ void enterArrayData(double* arr, int size) {
 }
 
 void outputArrayData(const double* arr, int size) {
-    cout << "Array values are:" << endl;
+    cout << "Array values are: [ " << endl;
     for (int i = 0; i < size; ++i) {
         cout << *(arr + i) << " "; 
     }
-    cout << endl;
+    cout << " ]" << endl;
 }
