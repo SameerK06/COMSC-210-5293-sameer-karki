559d3349fa2702cce8936f1b2f9e216df616a1ac
Sameer Karki <sameerkarkeetheone@gmail.com>
2026-09-25 18:56:47 -0700

    Finished working on the output file, working on displaying the results

--- Lab 13: Student Grade Sorter/sorter.cpp
+++ Lab 13: Student Grade Sorter/sorter.cpp
@@ -15,10 +15,11 @@ struct Student {
 
 int main() {
     Student student_arr[MAX_STUDENTS];
-    string filename = "210-lab-13-grades.txt";
-    ifstream inFile(filename);
+    string inputFile = "210-lab-13-grades.txt";
+    string outputFile = "210-lab-13-grades-sorted.txt";
+    ifstream inFile(inputFile);
     if (!inFile) {
-        cout << "Error: Cannot open input file " << filename << endl;
+        cout << "Error: Cannot open input file " << inputFile << endl;
         return 1;
     }
     int count = 0;
@@ -27,7 +28,7 @@ int main() {
     }
     inFile.close();
     if (count <= 0) {
-        cout << "No records found or error reading file " << filename << endl;
+        cout << "No records found or error reading file " << inputFile << endl;
         return 1;
     } else {
         cout << "Read " << count << " student records" << endl;
@@ -47,15 +48,32 @@ int main() {
         }
     }
 
-    ofstream outFile(filename);
+    ofstream outFile(outputFile);
     if (!outFile) {
-        cout << "Error: Cannot open output file " << filename << endl;
+        cout << "Error: Cannot open output file " << outputFile << endl;
         return 1;
     }
     for (int i = 0; i < count; i++) {
-        outFile << student_arr[i] << " " <<
+        outFile << student_arr[i].id << " " << student_arr[i].score << "\n";
     }
+    outFile.close();
+    cout << "The sorted results are saved to " << outputFile << endl;
 
+    double mean = 0.0;
+    double minScore, maxScore = student_arr[0].score;
+    int minId, maxId;
+    for (int i = 0; i < count; i++) {
+        sum += student_arr[i].score;
+        if (student_arr[i].score < minScore) {
+            minScore = student_arr[i].score;
+            minId = student_arr[i].id;
+        }
+        if (student_arr[i].score > maxScore) {
+            maxScore = student_arr[i].score;
+            maxId = student_arr[i].id;
+        }
+    }
+    mean/=count;
 
 
 
